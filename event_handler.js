595974d49ff574d3c85da9043bdc921622bdb47e
Sudhir919 <104786200+Sudhir919@users.noreply.github.com>
2023-01-18 11:22:48 +0530

    Create event_handler.js

---
+++ event_handler.js
@@ -0,0 +1,48 @@
+
+
+//*****************************************html ************************
+/*
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Listener</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <v>
+
+    <button id="btn" >Click Me</button>
+
+    </div>
+    <script src="script.js"></script>
+    
+</body>
+</html>
+
+
+
+
+*/
+
+
+
+
+
+btn.addEventListener('click',function(){
+
+   alert("hello world1"); 
+})
+
+btn.addEventListener('click',function(){
+
+   alert("hello world2"); 
+})
+
+
+
