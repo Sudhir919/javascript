e4766427319109658fabac20d54a913ea690d868
Sudhir919 <104786200+Sudhir919@users.noreply.github.com>
2023-03-26 20:20:10 +0530

    Create 07_Async - Await.js

---
+++ 07_Async - Await.js
@@ -0,0 +1,50 @@
+
+
+// Reference:- https://www.programiz.com/javascript/async-await
+
+
+// for normal function
+
+  function sudhir(){
+    
+    return "sudhir";
+  }
+
+  let a = sudhir();
+  console.log(a)     // it will return sudhir 
+
+// on using async before function
+
+ async function sudhir(){
+    
+    return "sudhir";
+  }
+
+  let a = sudhir();
+  console.log(a)    // now it return promise
+
+
+//  ************************* The syntax of async function is: **********************************
+
+async function name(parameter1, parameter2, ...paramaterN) {
+    // statements
+}
+
+
+// *********************************The syntax to use await is ****************************88888
+
+let result = await promise;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
