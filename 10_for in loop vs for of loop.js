715ed85995d68eddfe978deb16d0b3e126c41755
Sudhir919 <104786200+Sudhir919@users.noreply.github.com>
2023-03-28 19:07:36 +0530

    Create 10_for in loop vs for of loop.js

---
+++ 10_for in loop vs for of loop.js
@@ -0,0 +1,64 @@
+
+// **************************** for in loop ***************************************
+
+/*  it mainly deal with index */
+
+
+a = ["rohit","virat","bumrah","jaddu","chahal"];
+
+for(let name in a){
+    
+    console.log(name);
+}
+
+
+/* -->> Output
+
+   0
+   1
+   2
+   3
+   4
+
+ */
+ 
+
+// ********************* for of loop ****************************
+
+/* it deal with item present at index */
+
+
+b = ["rohit","virat","bumrah","jaddu","chahal"];
+for(let name of b){
+
+    console.log(name);
+}
+
+/* -->> Output
+
+ rohit
+ virat
+ bumrah
+ jaddu
+ chahal
+ 
+ 
+ */
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
